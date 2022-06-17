7ca77cf3f343cb28ebd93a4747506fed867e83b9
jeonniga <34230455+jeonniga@users.noreply.github.com>
2022-06-17 23:09:25 +0900

    Update changer.py

--- changer.py
+++ changer.py
@@ -8,12 +8,14 @@ filePathWithSlash = filePath + "\\"
 s = sched.scheduler(time.time, time.sleep)
 def do_something(sc): 
     for counter, filename in enumerate(os.listdir(directoryName)):
-        
-        srcfilenameWithPath = os.path.join(filePathWithSlash, filename)
-        neofilename = filename[0:17]
-        dstfilenameWithPath = os.path.join(filePathWithSlash, neofilename + '.mp4')
-        print(filename, '->', neofilename + '.mp4')
-        os.rename(srcfilenameWithPath, dstfilenameWithPath )
+        try:
+            srcfilenameWithPath = os.path.join(filePathWithSlash, filename)
+            neofilename = filename[0:17]
+            dstfilenameWithPath = os.path.join(filePathWithSlash, neofilename + '.mp4')
+            print(filename, '->', neofilename + '.mp4')
+            os.rename(srcfilenameWithPath, dstfilenameWithPath )
+        except:
+            pass
     sc.enter(60, 1, do_something, (sc,))
 
 s.enter(60, 1, do_something, (s,))
